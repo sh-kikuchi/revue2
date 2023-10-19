3e83675a798208582fc4d2802f9ae311973fb0af
sh-kikuchi <shu94hist726@gmail.com>
2023-10-19 23:02:17 +0900

    feature:sounds_20231019

--- components/sounds/MusicalScale.vue
+++ components/sounds/MusicalScale.vue
@@ -1,20 +1,30 @@
 <template>
   <section class="note">
-    <input type="radio" value="1n"  v-model="note" /><label for="1n">全音符</label>
-    <input type="radio" value="2n"  v-model="note" /><label for="2n">2分音符</label>
-    <input type="radio" value="4n"  v-model="note" /><label for="4n">4分音符</label>
-    <input type="radio" value="8n"  v-model="note" /><label for="8n">8分音符</label>
-    <input type="radio" value="16n" v-model="note" /><label for="16n">16分音符</label>
+    <div>
+      <input type="radio" value="1n"  v-model="note" /><label for="1n">全音符</label>
+    </div>
+    <div>
+      <input type="radio" value="2n"  v-model="note" /><label for="2n">2分音符</label>
+    </div>
+    <div>
+      <input type="radio" value="4n"  v-model="note" /><label for="2n">4分音符</label>
+    </div>
+    <div>
+      <input type="radio" value="8n"  v-model="note" /><label for="8n">8分音符</label>
+    </div>
+    <div>
+      <input type="radio" value="16n"  v-model="note" /><label for="16n">16分音符</label>
+    </div>
   </section>
   <div class="scale">
-    <input type="button" value="ド"   @click="sound('C4')">
-    <input type="button" value="レ"   @click="sound('D4')">
-    <input type="button" value="ミ"   @click="sound('E4')">
-    <input type="button" value="ファ" @click="sound('F4')">
-    <input type="button" value="ソ"   @click="sound('G4')">
-    <input type="button" value="ラ"   @click="sound('A4')">
-    <input type="button" value="シ"   @click="sound('B4')">
-    <input type="button" value="ド"   @click="sound('C5')">
+    <div><input type="button" value="ド"   @click="sound('C4')"></div>
+    <div><input type="button" value="レ"   @click="sound('D4')"></div>
+    <div><input type="button" value="ミ"   @click="sound('E4')"></div>
+    <div><input type="button" value="ファ" @click="sound('F4')"></div>
+    <div><input type="button" value="ソ"   @click="sound('G4')"></div>
+    <div><input type="button" value="ラ"   @click="sound('A4')"></div>
+    <div><input type="button" value="シ"   @click="sound('B4')"></div>
+    <div><input type="button" value="ド"   @click="sound('C5')"></div>
   </div>
 </template>
 <script>
@@ -37,5 +47,16 @@ export default {
 input[type="button"]{
   width: 50px;
   height: 50px;
+  border: 2px dotted white;
+}
+.note, 
+.scale{
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+.note>div,
+.scale>div{
+  margin: 5px;
 }
 </style>
