697dc1197a9ecfe53c3cc31f508405a5269ba17b
sh-kikuchi <shu94hist726@gmail.com>
2023-11-16 22:56:46 +0900

    make:components_textarea_20231116

---
+++ components/public/atoms/textfield/TextArea.vue
@@ -0,0 +1,84 @@
+<template>
+  <textarea
+    :id    = "id"
+    :class = "class"
+    :name  = "name"
+    :disabled = "disabledBool"
+    :readonly = "readonlyBool"
+    :maxlength="maxlength"
+    :minlength="minlength"
+    :rows="rows"
+    :cols="cols"
+    v-model= "valueTxtRef"
+    @input="$emit('update:valueTxt', valueTxtRef)"
+  
+  ></textarea>
+</template>
+<script setup>
+const valueTxtRef = ref(props.valueTxt);
+const props = defineProps({
+  id: {
+    type: String,
+    default: ''
+  },
+  class: {
+    type: String,
+    default: ''
+  },
+  name: {
+    type: String,
+    default: ''
+  },
+  text:{
+    type: String,
+    default: ''
+  },
+  disabledBool: {
+    type: Boolean,
+    default: false
+  },
+  readonlyBool: {
+    type: Boolean,
+    default: false
+  },
+  placeholder: {
+    type: String,
+    default: ''
+  },
+  maxlength: {
+    type: Number,
+    default: null
+  },
+  minlength: {
+    type: Number,
+    default: 0
+  },
+  rows: {
+    type: Number,
+    default: 5
+  },
+  cols: {
+    type: Number,
+    default: 30
+  },
+  valueTxt: {
+    type: String,
+    default: ''
+  },
+})
+
+</script>
+<style scoped>
+textarea{
+ padding: 2px;
+ background-color: whitesmoke;
+}
+textarea[readonly], textarea[disabled] {
+color: #417bbc;
+background-color: #E2E8E7;
+cursor: not-allowed;
+}
+
+
+
+</style>
---
+++ pages/factories/components.vue
@@ -0,0 +1,19 @@
+<script setup>
+import TextArea from "../../components/public/atoms/textfield/TextArea"
+import FormInput from "~~/components/public/atoms/inputs/FormInput.vue"
+import FormRadio from "~~/components/public/atoms/inputs/FormRadio.vue"
+import FormCheckBox from "~~/components/public/atoms/inputs/FormCheckBox.vue"
+import FormSelect from "~~/components/public/atoms/inputs/FormSelect.vue"
+
+const txt =ref("");
+
+</script>
+<template>
+
+    <TextArea
+      v-model:value-txt="txt" 
+    ></TextArea>
+   {{ txt }}
+
+
+</template>
